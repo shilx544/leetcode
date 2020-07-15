84b6b2914df953c6e2da200357f7daae4ca998e4
alexxcshi <alexxcshi@tencent.com>
2020-07-15 18:03:25 +0800

    add

---
+++ 面试题 17.13. 恢复空格.cpp
@@ -0,0 +1,15 @@
+// 输入：
+// dictionary = ["looked","just","like","her","brother"]
+// sentence = "jesslookedjustliketimherbrother"
+// 输出： 7
+// 解释： 断句后为"jess looked just like tim her brother"，共7个未识别字符。
+
+class Solution {
+public:
+    int respace(vector<string>& dictionary, string sentence) {
+        if (sentence.empty()) {
+            return 0;
+        }
+        
+    }
+};
