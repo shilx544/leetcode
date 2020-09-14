d2b5c19df7c89dea1e908725de3cd76f7fa449c6
alexxcshi <alexxcshi@tencent.com>
2020-09-14 23:30:39 +0800

    add

---
+++ 二叉树/94. 二叉树的中序遍历.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    void helper(TreeNode* root, vector<int>& result) {
+        if (root->left) {
+            helper(root->left, result);
+        }
+
+        if (root) {
+            result.push_back(root->val);
+        }
+
+        if (root->right) {
+            helper(root->right, result);
+        }
+
+        return;
+    }
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+
+        if (!root) {
+            return result;
+        }
+        helper(root, result);
+        return result;
+    }
+
+    // 迭代
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*> S;
+        vector<int> v;
+        TreeNode* rt = root;
+        while(rt || S.size()) {
+            while(rt) {
+                S.push(rt);
+                rt=rt->left;
+            }
+            rt = S.top();S.pop();
+            v.push_back(rt->val);
+            rt = rt->right;
+        }
+        return v;        
+    }
+};
