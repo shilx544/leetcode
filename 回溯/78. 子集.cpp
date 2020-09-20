38f54059de6ec551ff817dbf2e799ca841f3b5b8
alexxcshi <alexxcshi@tencent.com>
2020-09-20 23:56:37 +0800

    add

---
+++ 回溯/78. 子集.cpp
@@ -0,0 +1,20 @@
+class Solution {
+private:
+    vector<vector<int>> result;
+    vector<int> path;
+    void backtracking(vector<int>& nums, int startIndex) {
+        result.push_back(path);
+        for (int i = startIndex; i < nums.size(); i++) {
+            path.push_back(nums[i]);
+            backtracking(nums, i + 1);
+            path.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        result.clear();
+        path.clear();
+        backtracking(nums, 0);
+        return result;
+    }
+};
