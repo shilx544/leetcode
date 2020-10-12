639dac8d5661a7dd7c64b10d787a5d9addcd5d26
Alex SHI <alexshi@AlexdeMacBook-Pro.local>
2020-10-12 10:39:33 +0800

    add 75

--- 数组/75. 颜色分类.cpp
+++ 数组/75. 颜色分类.cpp
@@ -1,7 +1,38 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int num1, num2, num3 = 0;
-        
+        int zero = 0;
+        int one = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] == 0) {
+                swap(nums[i], nums[zero]);
+                if (zero < one) {
+                    swap(nums[i], nums[one]);
+                }
+                zero++;
+                one++;
+            } else if (nums[i] == 1) {
+                swap(nums[i], nums[one]);
+                one++;
+            }
+        }
+
+    }
+
+    void sortColors2(vector<int>& nums) {
+        int zero = 0;
+        int two = nums.size()-1;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            while (i <= two && nums[i] == 2) {
+                swap(nums[i], nums[two]);
+                two--;
+            }
+
+            if (nums[i] == 0) {
+                swap(nums[i], nums[zero]);
+                zero++;
+            }
+        }
     }
 };
