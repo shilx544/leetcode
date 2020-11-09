b639ad406694c66823b7c52e181e22cf1b0dd1ae
Alex SHI <alexshi@AlexdeMacBook-Pro.local>
2020-11-09 23:25:36 +0800

    add

--- 数组/973. 最接近原点的 K 个点.cpp
+++ 数组/973. 最接近原点的 K 个点.cpp
@@ -13,3 +13,39 @@ public:
         return result;
     }
 };
+
+// quick sort
+class Solution {
+private:
+    mt19937 gen{random_device{}()};
+
+public:
+    void random_select(vector<vector<int>>& points, int left, int right, int K) {
+        int pivot_id = uniform_int_distribution<int>{left, right}(gen);
+        int pivot = points[pivot_id][0] * points[pivot_id][0] + points[pivot_id][1] * points[pivot_id][1];
+        swap(points[right], points[pivot_id]);
+        int i = left - 1;
+        for (int j = left; j < right; ++j) {
+            int dist = points[j][0] * points[j][0] + points[j][1] * points[j][1];
+            if (dist <= pivot) {
+                ++i;
+                swap(points[i], points[j]);
+            }
+        }
+        ++i;
+        swap(points[i], points[right]);
+        // [left, i-1] 都小于等于 pivot, [i+1, right] 都大于 pivot
+        if (K < i - left + 1) {
+            random_select(points, left, i - 1, K);
+        }
+        else if (K > i - left + 1) {
+            random_select(points, i + 1, right, K - (i - left + 1));
+        }
+    }
+
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        int n = points.size();
+        random_select(points, 0, n - 1, K);
+        return {points.begin(), points.begin() + K};
+    }
+};
