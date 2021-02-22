dc1056518809bc9154dbc0a35d2c7570999433cc
Alex SHI <alexshi@AlexdeMacBook-Pro.local>
2021-02-22 23:48:53 +0800

    add

---
+++ 数组/1004. 最大连续1的个数 III.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& A, int K) {
+        int res = 0, zeros = 0, left = 0;
+        for (int right = 0; right < A.size(); ++right) {
+            if (A[right] == 0) ++zeros;
+            while (zeros > K) {
+                if (A[left++] == 0) --zeros;
+            }
+            res = max(res, right - left + 1);
+        }
+        return res;
+    }
+};
